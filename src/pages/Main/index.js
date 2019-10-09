bc8627377bd3ce6c5347177d8e8d96b2c2f4aada
Marocs <marcosrlj@outlook.com>
2019-10-09 12:13:02 -0300

    Adding qr code, main page card and menu; Tabs component;

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '../../components/Header';
+import Menu from '../../components/Menu';
+import Tabs from '../../components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
-      <Text>Nobanco</Text>
+
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#999" />
+            <Icon name="visibility-off" size={28} color="#999" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 37.519,62</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 12.300,00 recebida de Marcos Roberto quarta
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
     </Container>
   );
 }
